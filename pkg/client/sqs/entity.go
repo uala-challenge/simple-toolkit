0cc8416d01b23ae3326f370f08dfd1bdd73414dc
Fredy Hernández <hfredy717@outlook.com>
2025-03-17 14:54:53 -0500

    :green_heart: :sparkles: add functions to sqs

--- pkg/client/sqs/entity.go
+++ pkg/client/sqs/entity.go
@@ -12,4 +12,5 @@ type Config struct {
 
 type Service interface {
 	ReceiveMessage(ctx context.Context, params *sqs.ReceiveMessageInput, optFns ...func(*sqs.Options)) (*sqs.ReceiveMessageOutput, error)
+	DeleteMessage(ctx context.Context, params *sqs.DeleteMessageInput, optFns ...func(*sqs.Options)) (*sqs.DeleteMessageOutput, error)
 }
--- pkg/client/sqs/mock/service.go
+++ pkg/client/sqs/mock/service.go
@@ -14,6 +14,43 @@ type Service struct {
 	mock.Mock
 }
 
+// DeleteMessage provides a mock function with given fields: ctx, params, optFns
+func (_m *Service) DeleteMessage(ctx context.Context, params *sqs.DeleteMessageInput, optFns ...func(*sqs.Options)) (*sqs.DeleteMessageOutput, error) {
+	_va := make([]interface{}, len(optFns))
+	for _i := range optFns {
+		_va[_i] = optFns[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, params)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for DeleteMessage")
+	}
+
+	var r0 *sqs.DeleteMessageOutput
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *sqs.DeleteMessageInput, ...func(*sqs.Options)) (*sqs.DeleteMessageOutput, error)); ok {
+		return rf(ctx, params, optFns...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *sqs.DeleteMessageInput, ...func(*sqs.Options)) *sqs.DeleteMessageOutput); ok {
+		r0 = rf(ctx, params, optFns...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*sqs.DeleteMessageOutput)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *sqs.DeleteMessageInput, ...func(*sqs.Options)) error); ok {
+		r1 = rf(ctx, params, optFns...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ReceiveMessage provides a mock function with given fields: ctx, params, optFns
 func (_m *Service) ReceiveMessage(ctx context.Context, params *sqs.ReceiveMessageInput, optFns ...func(*sqs.Options)) (*sqs.ReceiveMessageOutput, error) {
 	_va := make([]interface{}, len(optFns))
